175939b5a815d199e22b851102a6133c6e59ff5e
Omar Hosny <102857390+Omar-Hosny1@users.noreply.github.com>
2024-01-23 14:08:23 +0200

    spring boot manual crud

--- src/main/java/com/learnspring/employeessystem/EmployeesSystemApplication.java
+++ src/main/java/com/learnspring/employeessystem/EmployeesSystemApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EmployeesSystemApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(EmployeesSystemApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeesSystemApplication.class, args);
+    }
 }
