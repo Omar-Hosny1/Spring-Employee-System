4a1514596c91e5ee5100893f89d8dda80d5806c5
Omar Hosny <102857390+Omar-Hosny1@users.noreply.github.com>
2024-01-29 22:28:34 +0200

    dsc

--- src/main/java/com/learnspring/employeessystem/dao/EmployeeRepository.java
+++ src/main/java/com/learnspring/employeessystem/dao/EmployeeRepository.java
@@ -2,6 +2,9 @@ package com.learnspring.employeessystem.dao;
 
 import com.learnspring.employeessystem.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+// @RepositoryRestResource(path = "members") // to change the endpoint path so instead of employees it will be members.
+// note that the name come from the entity name then add to it 's'
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }
